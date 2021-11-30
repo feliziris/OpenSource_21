19acdd2ce84f274d9a313c5af78761e4f766d274
feliziris <85056582+feliziris@users.noreply.github.com>
2021-12-01 00:08:02 +0900

    Add files via upload

---
+++ lab2/lab2-4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Linux is fun?"
+read text
+case $text in
+"Y") echo "yes";;
+"N") echo "no";;
+"yes") echo "yes";;
+"no") echo "no";;
+*) echo "Please enter yes(Y) or no(N)";;
+esac
